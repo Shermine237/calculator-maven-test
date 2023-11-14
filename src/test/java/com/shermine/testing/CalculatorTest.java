9200be8f033391b5ee387536eb695cc281ef5106
Charlie Rostant YOSSA <charlieyossa@gmail.com>
2023-11-14 15:53:19 +0100

    First commit

---
+++ src/test/java/com/shermine/testing/CalculatorTest.java
@@ -0,0 +1,13 @@
+package com.shermine.testing;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest
+{
+    @Test
+    void test()
+    {
+        fail("Not yet implemented");
+    }
+}
