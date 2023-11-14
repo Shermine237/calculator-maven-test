0e31e868a34d63bb6a0a611c417ca9bb7b5279e1
Charlie Rostant YOSSA <charlieyossa@gmail.com>
2023-11-14 16:49:17 +0100

    Add one test

--- src/main/java/com/shermine/Calculator.java
+++ src/main/java/com/shermine/Calculator.java
@@ -1,2 +1,7 @@
 package com.shermine;
 
+public class Calculator {
+    public int addition(int a, int b) {
+        return a+b;
+    }
+}
--- src/test/java/com/shermine/testing/CalculatorTest.java
+++ src/test/java/com/shermine/testing/CalculatorTest.java
@@ -1,4 +1,5 @@
 package com.shermine.testing;
+import com.shermine.Calculator;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -6,8 +7,17 @@ import static org.junit.jupiter.api.Assertions.*;
 public class CalculatorTest
 {
     @Test
-    void test()
+    void testAddTwoPositiveNumbers()
     {
-        fail("Not yet implemented");
+        // ARRANGE
+        final int a = 1;
+        final int b = 4;
+        final Calculator calculator = new Calculator();
+
+        // ACT
+        final int somme = calculator.addition(a, b);
+
+        // ASSERT
+        assertEquals(5, somme);
     }
 }
