0b0013346c754f3f38b1954dbd81e70137b91e2c
Charlie Rostant YOSSA <charlieyossa@gmail.com>
2023-11-15 13:38:32 +0100

    Use @BeforeEach to initialise attributs of Class CalculatorTest

--- src/test/java/com/shermine/testing/CalculatorTest.java
+++ src/test/java/com/shermine/testing/CalculatorTest.java
@@ -1,23 +1,46 @@
 package com.shermine.testing;
 import com.shermine.Calculator;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest
 {
+    private Calculator calculatorUnderTest;
+
+    @BeforeEach
+    // A executer avant @Test
+    public void initCalculator() {
+        System.out.println("Appel avant chaque test");
+        // Initialisation des attributs de la Cette classe CalculatorTest
+        this.calculatorUnderTest = new Calculator();
+    }
     @Test
     void testAddTwoPositiveNumbers()
     {
         // ARRANGE
         final int a = 1;
         final int b = 4;
-        final Calculator calculator = new Calculator();
 
         // ACT
-        final int somme = calculator.addition(a, b);
+        final int result = this.calculatorUnderTest.addition(a, b);
 
         // ASSERT
-        assertEquals(5, somme);
+        assertEquals(5, result);
+    }
+
+
+    @Test
+    void testMultiTwoPositiveNumbers()
+    {
+        // ARRANGE
+        final int a = 2;
+        final int b = 4;
+
+        // ACT
+        final int result = this.calculatorUnderTest.multiply(a, b);
+
+        // ASSERT
+        assertEquals(8, result);
     }
 }
